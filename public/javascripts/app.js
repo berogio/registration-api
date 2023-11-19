7655d8abbbf79c861ee28a278edc6213895a9897
berogio <giorgi.beridze01e@geolab.edu.ge>
2023-11-19 21:41:17 +0100

    AddedLoginPostRoute

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,13 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
     registrationForm.addEventListener("submit", function(event) {
         event.preventDefault();
 
-        // Holen Sie sich die eingegebenen Werte aus dem Formular
         const vorname = document.getElementById("vorname").value;
         const nachname = document.getElementById("nachname").value;
         const email = document.getElementById("email").value;
         const password = document.getElementById("passwort").value;
 
-        // Erstellen Sie ein JavaScript-Objekt mit den Daten
+
         const formData = {
             vorname: vorname,
             nachname: nachname,
@@ -18,12 +17,9 @@ document.addEventListener("DOMContentLoaded", function() {
             password: password
         };
 
-        // Hier können Sie die Daten überprüfen und ggf. Validierung durchführen
 
-        // Ersetzen Sie 'ZIEL_SERVER_URL' durch die tatsächliche URL Ihres Zielservers
         const zielUrl = 'http://localhost:3000/register';
 
-        // Senden Sie die Daten an den Server
         fetch(zielUrl, {
                 method: 'POST',
                 headers: {
@@ -33,16 +29,54 @@ document.addEventListener("DOMContentLoaded", function() {
             })
             .then(response => response.json())
             .then(data => {
-                // Hier können Sie die Serverantwort verarbeiten
+
                 console.log('Serverantwort:', data);
-                // Weiterleitung zur Anmeldeseite nach erfolgreicher Registrierung
+
                 if (data) {
                     window.location.href = 'login.html';
                 }
             })
             .catch(error => {
                 console.error('Fehler beim Senden der Daten:', error);
-                // Hier können Sie Fehlerbehandlung hinzufügen
+
+            });
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const registrationForm = document.getElementById("LogiForm");
+
+    registrationForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const loginEmail = document.getElementById('LoginEmail').value
+        const loignPassword = document.getElementById('LoginPasswort').value
+
+        const LoginFormData = {
+            loginEmail: loginEmail,
+            loignPassword: loignPassword
+        }
+
+        const zielUrl = 'http://localhost:3000/login';
+
+        fetch(zielUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(LoginFormData)
+            })
+            .then(response => response.json())
+            .then(data => {
+
+                console.log('Serverantwort:', data);
+
+                if (data) {
+                    window.location.href = 'login.html';
+                }
+            })
+            .catch(error => {
+                console.error('Fehler beim Senden der Daten:', error);
+
             });
     });
 });
--- routes/login.js
+++ routes/login.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-// Hier sollten Sie Ihre Datenbank- oder Speicherlogik einbinden
-// Zum Beispiel: const database = require('../database'); 
 
-// Route zum Anzeigen der Anmeldeseite
 router.get('/login', function(req, res, next) {
-    res.sendFile('login.html', { root: 'public' }); // Verwenden Sie hier die entsprechende Ansicht (View) für die Anmeldeseite
+    res.sendFile('login.html', { root: 'public' });
 });
 
-// Route zum Verarbeiten des Anmeldeformulars
-router.post('/login', function(req, res, next) {
-    // Hier sollten Sie die Anmelde-Authentifizierungslogik implementieren
-    const { email, passwort } = req.body;
 
-    // Überprüfen Sie die Benutzeranmeldeinformationen in der Datenbank
+router.post('/login', async(req, res, next) => {
 
-    // Wenn die Anmeldung erfolgreich ist, können Sie den Benutzer zu einer Dashboard-Seite umleiten
+    const { loignPassword, loginEmail } = req.body
+
+
 
 });
 
