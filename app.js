e39434b9e1e7397a2d076970b293364e47690630
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-02-02 02:25:33 +0100

    test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     //es
-    cookie: { httpOnly: true, secure: false }
+    cookie: { httpOnly: true, secure: true }
 }))
 
 app.use(cors())
